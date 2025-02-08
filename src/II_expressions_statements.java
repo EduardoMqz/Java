bdf214db5e72581a7f7d7c68530234f0ca791ee9
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-02-07 21:42:45 -0600

    challenges

--- src/II_expressions_statements.java
+++ src/II_expressions_statements.java
@@ -8,38 +8,37 @@ public class II_expressions_statements {
         methodCalculateScore(true, 8, 200, 800);
         System.out.printf("Your final score was %s%n", methodCalculateScore(true, 8, 200, 800));
         methodChallenge("Tim", 5, 1111);
+        checkNumber(5);
+        System.out.println(toMilesPerHour(25.42));
+        printMegaBytesAndKiloBytes(2500);
 
     }
 
-    public static void keywordsAndExpressions(){
-        double kilometer = (100 * 1.609344);//2 expressions
+    public static void keywordsAndExpressions() {
+        double kilometer = (100 * 1.609344);// 2 expressions
         int highScore = 50;// 1 expression
-        if ( highScore > 25){// 1 expression
-            highScore = 1000 + highScore;//2 expressions
+        if (highScore > 25) {// 1 expression
+            highScore = 1000 + highScore;// 2 expressions
         }
         int health = 1000;//
         if ((health < 25) && (highScore > 1000)) {// 6 expression in this 3 lines
-            highScore = highScore -1000;//
+            highScore = highScore - 1000;//
         }
     }
 
-    public static void statementsWhiteSpace(){
-        int                  myVariable             = 
-        50;
-        myVariable
-         ++;
-        myVariable     --
-        ;
-        System.
-        out.
-        println                             ("this is a test");
-        int anotherVariable = 50; myVariable--;
+    public static void statementsWhiteSpace() {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("this is a test");
+        int anotherVariable = 50;
+        myVariable--;
         if (myVariable == 0) {
             System.out.println("Its now zero");
         }
     }
 
-    public static void ifThenElseControl(){
+    public static void ifThenElseControl() {
         boolean gameOver = true;
         int score = 4000;
         int levelCompleted = 5;
@@ -49,19 +48,19 @@ public class II_expressions_statements {
         }
         if (score <= 5000) {
             System.out.println("your score was less than 5000");
-        }else{
+        } else {
             System.out.println("go here");
         }
         if (score < 5000 && score > 1000) {
             System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000){
+        } else if (score < 1000) {
             System.out.println("Your score was less n 1000");
-        }else{
+        } else {
             System.out.println("go here");
         }
     }
 
-    public static void ifThenElseChallenge(int score){
+    public static void ifThenElseChallenge(int score) {
         boolean gameOver = true;
         int levelCompleted = 8;
         int bonus = 200;
@@ -72,24 +71,49 @@ public class II_expressions_statements {
         }
     }
 
-    public static int methodCalculateScore(boolean gameOver, int levelCompleted, int bonus, int score){
+    public static int methodCalculateScore(boolean gameOver, int levelCompleted, int bonus, int score) {
         if (gameOver) {
-            score += (levelCompleted * bonus);   
+            score += (levelCompleted * bonus);
         }
         return score;
     }
 
-    public static void methodChallenge(String name, int position, int score){
+    public static void methodChallenge(String name, int position, int score) {
         System.out.printf("%s managed to get into position %d on the high score list%n", name, position);
+        int positionScore = 4;
         if (score >= 1000) {
-            System.out.println("1");
-        }else if (score >= 500 && score < 1000) {
-            System.out.println("2");
-        }else if (score >= 100 && score < 500) {
-            System.out.println("3");
+            positionScore = 1;
+        } else if (score >= 500) {
+            positionScore = 2;
+        } else if (score >= 100) {
+            positionScore = 3;
+        }
+        System.out.println(positionScore);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static int toMilesPerHour(double kilometerPerHour) {
+        if (kilometerPerHour < 0) {
+            return -1;
+        } else {
+            return (int) Math.round(kilometerPerHour / 1.609);
+        }
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid value");
         }else{
-            System.out.println("4");
+            System.out.printf("%s KB = %s MB and %s KB",kiloBytes, kiloBytes / 1024, kiloBytes % 1024);
         }
     }
 }
-
