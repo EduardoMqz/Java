28677b570ff2344efd435a8d38273d7693721b9b
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-03-20 20:13:37 -0600

    arrays

--- src/VI_Arrays.java
+++ src/VI_Arrays.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class VI_Arrays {
         public static void main(String[] args) throws Exception {
             arraysFoundations();
+            populatingLooping();
+            sortingFiling();
+            binarySearch();
+            arrayChallenge();
     
         }
 
@@ -20,11 +27,88 @@ public class VI_Arrays {
             int[] newArray;
             newArray = new int[]{5, 4, 3, 2, 1};
             for (int i = 0; i < newArray.length; i++) {
-                System.out.println(newArray[i] + " ");
+                System.out.print(newArray[i] + " ");
             }
-            System.out.println("**".repeat(10));
+            System.out.println("");
             for (int i : newArray) {
-                System.out.println(i + " ");
+                System.out.print(i + " ");
             }
         }
+
+        public static void populatingLooping(){
+            int[] newArray;
+            newArray = new int[5];
+            for (int i = 0; i < newArray.length; i++) {
+                newArray[i] = newArray.length - i;
+            }
+            System.out.println("");
+            for (int i = 0; i < newArray.length; i++) {
+                System.out.print(newArray[i] + " ");
+            }
+            System.out.println();
+            for (int element : newArray) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+            System.out.println(Arrays.toString(newArray));
+            Object objectVariable = newArray;
+            if (objectVariable instanceof int[]) {
+                System.out.println("objectVariable is really an int[]");
+            }
+            Object[] objectArray = new Object[3];
+            objectArray[0] = "Hello";
+            objectArray[1] = false;
+            objectArray[2] = newArray;
+        }
+
+        public static void sortingFiling(){
+            int[] firstArray = getRandomArray(10);
+            System.out.println(Arrays.toString(firstArray));
+            Arrays.sort(firstArray);
+            System.out.println(Arrays.toString(firstArray));
+            int[] secondArray = new int[10];
+            System.out.println(Arrays.toString(secondArray));
+            Arrays.fill(secondArray,5);
+            System.out.println(Arrays.toString(secondArray));
+            int[] thirdArray = Arrays.copyOf(firstArray, firstArray.length);
+            System.out.println(Arrays.toString(thirdArray));
+
+        }
+
+        private static int[] getRandomArray(int len){
+            Random random = new Random();
+            int[] newInt = new int[len];
+            for (int i = 0; i < len; i++) {
+                newInt[i] = random.nextInt(100);
+            }
+            return newInt;
+        }
+
+        public static void binarySearch(){
+            String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David", "Ann", "Alphonse", "Eve"};
+            Arrays.sort(sArray);
+            System.out.println(Arrays.toString(sArray));
+            if (Arrays.binarySearch(sArray, "Mark") >= 0) {
+                System.out.println("Found mark in the list");
+            }
+            int[] iArray1 = {4,5,2,3,6,8};
+            int[] iArray2 = {4,5,2,3,6,8};
+            if(Arrays.equals(iArray1, iArray2)){
+                System.out.println("Arrays are equal");
+            }else{
+                System.out.println("They are not equal");
+            }
+        }
+
+        public static void arrayChallenge(){
+            int[] arrayChallenge = getRandomArray(15);
+            System.out.println(Arrays.toString(arrayChallenge));
+            Arrays.sort(arrayChallenge);
+            int[] sortedArray =Arrays.copyOf(arrayChallenge, arrayChallenge.length);
+            for (int i = 0; i < arrayChallenge.length; i++) {
+                sortedArray[i] = arrayChallenge[arrayChallenge.length - (1 + i)];
+            }
+            System.out.println(Arrays.toString(sortedArray));
+        }
+
 }
